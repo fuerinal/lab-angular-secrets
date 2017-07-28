9abf0e98feb2ccec5c68623882520e3192fd0f45
Alejandro de la Fuente <alex695afr@gmail.com>
2017-07-28 15:25:00 +0200

    Iteration 6

---
+++ starter-code/authentication-app/src/app/routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import {  AuthSignupComponent } from './auth-signup/auth-signup.component';
+import {  AuthLoginComponent } from './auth-login/auth-login.component';
+import {  MyPrivatePageComponent } from './my-private-page/my-private-page.component';
+import {  AppComponent } from './app.component';
+
+
+export const routes: Routes = [
+
+    { path: 'signup', component:  AuthSignupComponent  },
+    { path: 'login', component: AuthLoginComponent  },
+      { path: 'private', component: MyPrivatePageComponent },
+    { path: '**', redirectTo: '' }
+];
--- starter-code/authentication-app/src/app/session.service.ts
+++ starter-code/authentication-app/src/app/session.service.ts
@@ -1,8 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import { environment } from '../environments/environment';
+
+export interface User{
+  _id:string,
+  username:string,
+  name:string,
+  secret:string,
+  updated_at:Date,
+  created_at:Date
+}
 
 @Injectable()
 export class SessionService {
-  constructor() { }
+  user: User; // The current logged in user
+  BASE_URL: string = `${environment.BASE_URL}/api/auth`;
+  options:Object = {withCredentials:true};
+
+  constructor(private http: Http) { }
+  handleError(e) {
+    console.error("Error en la llamada a la API");
+    return Observable.throw(e.json().message);
+  }
+
+  signup(username: string, password: string): Observable<User> {
+    return this.http.post(`${this.BASE_URL}/signup`, { username, password }, this.options)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  login(username: string, password: string): Observable<User> {
+    return this.http.post(`${this.BASE_URL}/login`, { username, password }, this.options)
+      .map(res => {
+        this.user = res.json();
+        return this.user;
+      })
+      .catch(this.handleError);
+  }
+
+  logout(): Observable<Object> {
+    return this.http.get(`${this.BASE_URL}/logout`)
+      .map(res => {
+        res.json();
+        this.user = undefined;
+      })
+      .catch(this.handleError);
+  }
+
+  isLoggedIn(): Observable<User> {
+    return this.http.get(`${this.BASE_URL}/loggedin`, this.options)
+      .map(res => {
+        this.user = res.json();
+        return this.user;
+      })
+      .catch(this.handleError);
+  }
 }
